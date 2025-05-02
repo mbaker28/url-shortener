60cba593702d2b947155a227bed633a0860f4f5f
Matthew Baker <mattbakerk@gmail.com>
2025-05-02 17:01:47 -0500

    Updates

---
+++ src/Entity/ShortUrl.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShortUrlRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ShortUrlRepository::class)]
+class ShortUrl
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $longUrl = null;
+
+    #[ORM\Column(length: 7)]
+    private ?string $shortCode = null;
+
+    #[ORM\Column]
+    private ?int $hits = null;
+
+    #[ORM\Column]
+    private ?\DateTime $created = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getLongUrl(): ?string
+    {
+        return $this->longUrl;
+    }
+
+    public function setLongUrl(string $longUrl): static
+    {
+        $this->longUrl = $longUrl;
+
+        return $this;
+    }
+
+    public function getShortCode(): ?string
+    {
+        return $this->shortCode;
+    }
+
+    public function setShortCode(string $shortCode): static
+    {
+        $this->shortCode = $shortCode;
+
+        return $this;
+    }
+
+    public function getHits(): ?int
+    {
+        return $this->hits;
+    }
+
+    public function setHits(int $hits): static
+    {
+        $this->hits = $hits;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTime
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTime $created): static
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+}
---
+++ src/Repository/ShortUrlRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ShortUrl;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<ShortUrl>
+ */
+class ShortUrlRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ShortUrl::class);
+    }
+
+    //    /**
+    //     * @return ShortUrl[] Returns an array of ShortUrl objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('s.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?ShortUrl
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
