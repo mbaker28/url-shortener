7387e00250baa455fd323088e7c9c691ead51908
Matthew Baker <mattbakerk@gmail.com>
2025-05-03 13:32:02 -0500

    Updates

--- assets/app.tsx
+++ assets/app.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './styles/app.css';
 
-const root = createRoot(document.getElementById('app')!);
+const root = createRoot(document.body);
 
 root.render(<h1>Hello, world!</h1>);
--- src/Controller/UrlShortenerController.php
+++ src/Controller/UrlShortenerController.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Exception\ShortenerException;
+use App\Service\ShortenerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 final class UrlShortenerController extends AbstractController
 {
     public function __construct(
+        private ShortenerService $shortener
     ) {
     }
 
@@ -19,5 +24,28 @@ final class UrlShortenerController extends AbstractController
     public function __invoke(): Response
     {
         return $this->render('url-shortener.html.twig');
+    }
+
+    #[Route('/url-shortener/create', name: 'url_shortener_create', methods: ['POST'])]
+    public function createShortUrl(Request $request): JsonResponse
+    {
+        $entity = $this->shortener->urlToShortUrl($request->request->getString('url'));
+
+        return $this->json([
+            'url' => $entity->getLongUrl(),
+            'shortCode' => $entity->getShortCode(),
+        ], 200, ['Content-Type' => 'application/json']);
+    }
+
+    #[Route('/{shortCode}', methods: ['GET'])]
+    public function redirectToLongUrl(string $shortCode): Response
+    {
+        try {
+            $entity = $this->shortener->shortCodeToUrl($shortCode);
+
+            return $this->redirect($entity->getLongUrl());
+        } catch (ShortenerException $e) {
+            return new Response($e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
     }
 }
